5587b86bc869db45a5ad3c24a0ae7da9eb7840e2
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2015-04-08 22:53:19 -0500

    add new files

---
+++ ConsoleApplicationModule1/ConsoleApplicationModule1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationModule1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //**** student information ***//
+            string firstName;
+            string lastName;
+            DateTime birthDate;
+            string addressLine1;
+            string addressLine2;
+            string city;
+            string stateOrProvince;
+            string zipOrPostal;
+            string country;
+
+            //**** professor information ***//
+            string professorName;
+            
+            // 
+        }
+    }
+}
