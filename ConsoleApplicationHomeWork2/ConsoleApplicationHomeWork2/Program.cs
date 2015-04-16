3506c17f69574d88319fe13339da49a8077e1b55
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2015-04-16 18:54:00 -0500

    add new files for homework 2 and 3

---
+++ ConsoleApplicationHomeWork2/ConsoleApplicationHomeWork2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApplicationHomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowNumber = 8;
+            int colNumber = 8;
+            string currentPattern;
+
+            for (int i = 0; i < rowNumber; i++)
+            {
+                if (i % 2 == 0) //rowNumber is even, starts with "X"
+                {
+                    currentPattern = "X";
+                }
+                else
+                {
+                    currentPattern = "O";
+                }
+                
+                for (int j = 0; j < colNumber; j++)
+                {
+                    Console.Write(currentPattern);
+                    if (currentPattern == "X")
+                    {
+                        currentPattern = "O";
+                    }
+                    else
+                    {
+                        currentPattern = "X";
+                    }
+                }
+
+                Console.Write("\n");  // next line
+            }
+            Console.ReadLine();
+        }
+    }
+}
